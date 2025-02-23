3f87feda00d43b6e46ead5ee59d99afb1cc89752
Bilin <bilinpattasseril@gmail.com>
2025-02-23 02:21:56 -0800

    Added Login and SignUp Page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -68,9 +68,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainPageUI(isTaskWindowVisible = false)
+            MainApp()
         }
     }
+}
+
+@Composable
+fun MainApp() {
+    var isLoggedIn by remember { mutableStateOf(false) }
+    var isSigningUp by remember { mutableStateOf(false) }
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    if (isLoggedIn) {
+        MainPageUI(isTaskWindowVisible = false)
+    } else if (isSigningUp) {
+        SignUpScreen(onSignUpSuccess = {
+            isSigningUp = false
+            isLoggedIn = true
+        }, onBackToLogin = {
+            isSigningUp = false
+        })
+    } else {
+        LoginSignUpScreen(onLoginSuccess = { isLoggedIn = true }, onNavigateToSignUp = { isSigningUp = true })
+    }
+}
 }
 
 @Composable
@@ -276,7 +304,7 @@ fun MainPageUI(isTaskWindowVisible: Boolean) {
             BuzzIconButton(
                 onClick = { isScrapbookVisible.value = !isScrapbookVisible.value },
                 modifier = Modifier
-                    .size(130.dp)
+                    .size(110.dp)
                     .align(Alignment.BottomEnd)
                     .padding(16.dp)
                     .offset(y = (-20).dp)
